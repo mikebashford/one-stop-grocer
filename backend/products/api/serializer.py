f82889bf6c58483f44e1c333dc78326f84beb407
mikebashford <mikebashford@gmail.com>
2024-03-08 13:52:07 -0600

    Resetting folder structure, creating empty project

--- backend/products/api/serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from ..models import Products
-
-class ProductsSerializer(ModelSerializer):
-    class Meta:
-        model = Products
-        fields = '__all__'
--- backend/users/api/serializers.py
+++
@@ -1,6 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from users.models import Users
-class UsersSerializer(ModelSerializer):
-    class Meta:
-        model = Users
-        fields = '__all__'
